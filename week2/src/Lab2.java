6c36b4547c0f4ca31753b1c0c3381b17f0bab5e9
İlker Öztürk <xentzenith@gmail.com>
2024-05-17 15:12:22 -0400

    main

---
+++ week2/src/Lab2.java
@@ -0,0 +1,33 @@
+public class Lab2 {
+    public static void main(String[] args) {
+        Lab2 lab2 = new Lab2();
+        ReportCard reportCard1 = lab2.new ReportCard("John Doe", "123456", 90.0f); // Use the instance to create the ReportCard objects
+        ReportCard reportCard2 = lab2.new ReportCard("Jane Doe", "654321", 95.0f);
+        ReportCard reportCard3 = lab2.new ReportCard("John Smith", "456789", 85.0f);
+        
+        System.out.println(reportCard1.getReport());
+        System.out.println(reportCard2.getReport());
+        System.out.println(reportCard3.getReport());
+    }
+
+    public class ReportCard {
+        String studentName;
+        String studentId;
+        float currentGrade;
+
+        public ReportCard(String studentName, String studentId, Float currentGrade){
+            this.studentName = studentName;
+            this.studentId = studentId;
+            this.currentGrade = currentGrade;
+        }
+
+
+        public String getReport(){
+            String report = studentName+", " + studentId +", "+ currentGrade;
+            return report;
+        }
+
+           
+    }
+
+}
